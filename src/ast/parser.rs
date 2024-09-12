7b5008d13d511cf895db820d55366f376d072abc
Ricardo Maurizio Paul <memaurifu14@hotmail.com>
2024-09-12 14:08:16 +0200

    more refactors, expr parser

--- src/ast/parser.rs
+++ src/ast/parser.rs
@@ -22,225 +22,6 @@ impl<'a> Parser<'a> {
         todo!("parse")
     }
 
-    fn lvalue(&mut self) -> Result<LValue, Error> {
-        // println!("lvalue");
-        match self.lexer.peek_mut() {
-            Some(Token::Identifier(v)) => {
-                let variable = mem::take(v);
-                let next = self.lexer.next();
-
-                // println!("identifier {}", v);
-
-                if next == Some(Token::LeftParen) {
-                    self.lexer.next();
-                    let index = self.expression()?;
-                    if self.lexer.peek() == Some(&Token::RightParen) {
-                        let res = Ok(LValue::ArrayElement {
-                            variable,
-                            index: Box::new(index.unwrap()),
-                        });
-
-                        self.lexer.next();
-
-                        res
-                    } else {
-                        Err(Error {
-                            kind: ErrorKind::MismatchedParentheses,
-                            line: 0, // TODO
-                        })
-                    }
-                } else {
-                    Ok(LValue::Variable(variable))
-                }
-            }
-            _ => {
-                // println!("expected identifier");
-                Err(Error {
-                    kind: ErrorKind::ExpectedIdentifier,
-                    line: 0, // TODO
-                })
-            }
-        }
-    }
-
-    fn term(&mut self) -> Result<Option<Expression>, Error> {
-        match self.lexer.peek_mut() {
-            Some(Token::Number(n)) => {
-                let res = Ok(Some(Expression::Number(*n)));
-                self.lexer.next();
-                res
-            }
-            Some(Token::Identifier(_)) => self.lvalue().map(|v| Some(Expression::LValue(v))),
-            Some(Token::String(s)) => {
-                let res = Ok(Some(Expression::String(mem::take(s))));
-                self.lexer.next();
-                res
-            }
-            Some(Token::LeftParen) => {
-                self.lexer.next();
-                let res = self.expression()?;
-                if self.lexer.peek() == Some(&Token::RightParen) {
-                    self.lexer.next();
-                    Ok(res)
-                } else {
-                    Err(Error {
-                        kind: ErrorKind::MismatchedParentheses,
-                        line: 0, // TODO
-                    })
-                }
-            }
-            _ => Ok(None),
-        }
-    }
-
-    // // unary + and -
-    fn factor(&mut self) -> Result<Option<Expression>, Error> {
-        // println!("factor");
-        if self.lexer.peek() == Some(&Token::Plus) || self.lexer.peek() == Some(&Token::Minus) {
-            let op = match self.lexer.next() {
-                Some(Token::Plus) => UnaryOperator::Plus,
-                Some(Token::Minus) => UnaryOperator::Minus,
-                _ => unreachable!(),
-            };
-
-            let operand = self.factor();
-            let operand = if let Some(operand) = operand? {
-                operand
-            } else {
-                return Err(Error {
-                    kind: ErrorKind::ExpectedExpression,
-                    line: 0, // TODO
-                });
-            };
-
-            Ok(Some(Expression::Unary {
-                op,
-                operand: Box::new(operand),
-            }))
-        } else {
-            self.term()
-        }
-    }
-
-    fn mul_div(&mut self) -> Result<Option<Expression>, Error> {
-        let mut left = if let Some(left) = self.factor()? {
-            left
-        } else {
-            return Ok(None);
-        };
-
-        while let Some(&Token::Star) | Some(&Token::Slash) = self.lexer.peek() {
-            let op = match self.lexer.next() {
-                Some(Token::Star) => BinaryOperator::Mul,
-                Some(Token::Slash) => BinaryOperator::Div,
-                _ => unreachable!(),
-            };
-
-            let right = self.factor();
-            let right = if let Some(right) = right? {
-                right
-            } else {
-                return Err(Error {
-                    kind: ErrorKind::ExpectedExpression,
-                    line: 0, // TODO
-                });
-            };
-
-            left = Expression::Binary {
-                left: Box::new(left),
-                op,
-                right: Box::new(right),
-            };
-        }
-
-        Ok(Some(left))
-    }
-
-    fn add_sub(&mut self) -> Result<Option<Expression>, Error> {
-        let mut left = if let Some(left) = self.mul_div()? {
-            left
-        } else {
-            return Ok(None);
-        };
-
-        while let Some(&Token::Plus) | Some(&Token::Minus) = self.lexer.peek() {
-            println!("add_sub");
-
-            let op = match self.lexer.next() {
-                Some(Token::Plus) => BinaryOperator::Add,
-                Some(Token::Minus) => BinaryOperator::Sub,
-                _ => unreachable!(),
-            };
-
-            let right = self.add_sub();
-            let right = if let Some(right) = right? {
-                right
-            } else {
-                return Err(Error {
-                    kind: ErrorKind::ExpectedExpression,
-                    line: 0, // TODO
-                });
-            };
-
-            left = Expression::Binary {
-                left: Box::new(left),
-                op,
-                right: Box::new(right),
-            };
-        }
-
-        Ok(Some(left))
-    }
-
-    fn comparison(&mut self) -> Result<Option<Expression>, Error> {
-        let mut left = if let Some(left) = self.add_sub()? {
-            left
-        } else {
-            return Ok(None);
-        };
-
-        while let Some(&Token::Equal)
-        | Some(&Token::Diamond)
-        | Some(&Token::LessThan)
-        | Some(&Token::LessOrEqual)
-        | Some(&Token::GreaterThan)
-        | Some(&Token::GreaterOrEqual) = self.lexer.peek()
-        {
-            let op = match self.lexer.next() {
-                Some(Token::Equal) => BinaryOperator::Eq,
-                Some(Token::Diamond) => BinaryOperator::Ne,
-                Some(Token::LessThan) => BinaryOperator::Lt,
-                Some(Token::LessOrEqual) => BinaryOperator::Le,
-                Some(Token::GreaterThan) => BinaryOperator::Gt,
-                Some(Token::GreaterOrEqual) => BinaryOperator::Ge,
-                _ => unreachable!(),
-            };
-
-            let right = self.comparison();
-            let right = if let Some(right) = right? {
-                right
-            } else {
-                return Err(Error {
-                    kind: ErrorKind::ExpectedExpression,
-                    line: 0, // TODO
-                });
-            };
-
-            left = Expression::Binary {
-                left: Box::new(left),
-                op,
-                right: Box::new(right),
-            };
-        }
-
-        Ok(Some(left))
-    }
-
-    fn expression(&mut self) -> Result<Option<Expression>, Error> {
-        // println!("expression");
-        self.comparison()
-    }
-
     // fn let_(&mut self) -> Result<Statement, Error> {
     //     // println!("let");
     //     let variable = match &mut self.current_token {
@@ -918,50 +699,317 @@ impl<'a> Parser<'a> {
     // }
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+mod expression {
+    use crate::ast::{
+        error::ErrorKind, node::LValue, BinaryOperator, Error, Expression, Lexer, Token,
+        UnaryOperator,
+    };
+    use std::{iter::Peekable, mem};
 
-    #[test]
-    fn test_add_sub_1() -> Result<(), Error> {
-        let expected = Expression::Binary {
-            left: Box::new(Expression::Number(1)),
-            op: BinaryOperator::Add,
-            right: Box::new(Expression::Binary {
-                left: Box::new(Expression::Number(2)),
-                op: BinaryOperator::Sub,
-                right: Box::new(Expression::Number(3)),
-            }),
-        };
-
-        let lexer = Lexer::new("1 + 2 - 3");
-        let mut parser = Parser::new(lexer);
-
-        let res = parser.add_sub()?;
-
-        if let Some(res) = res {
-            assert_eq!(res, expected, "Expected: {:?}, got: {:?}", expected, res);
-        } else {
-            return Err(Error {
-                kind: ErrorKind::ExpectedExpression,
-                line: 0,
-            });
-        }
-
-        Ok(())
+    pub struct ExpressionParser<'a> {
+        lexer: Peekable<Lexer<'a>>,
     }
 
-    #[test]
-    fn test_lvalue_1() -> Result<(), Error> {
-        let expected = LValue::Variable("A".to_owned());
+    impl<'a> ExpressionParser<'a> {
+        pub fn new(lexer: Peekable<Lexer<'a>>) -> Self {
+            Self { lexer }
+        }
 
-        let lexer = Lexer::new("A");
-        let mut parser = Parser::new(lexer);
+        pub fn parse(&mut self) -> Result<Option<Expression>, Error> {
+            // println!("expression");
+            self.comparison()
+        }
 
-        let res = parser.lvalue()?;
+        fn lvalue(&mut self) -> Result<LValue, Error> {
+            // println!("lvalue");
+            match self.lexer.peek_mut() {
+                Some(Token::Identifier(v)) => {
+                    let variable = mem::take(v);
+                    let next = self.lexer.next();
 
-        assert_eq!(res, expected, "Expected: {:?}, got: {:?}", expected, res);
+                    // println!("identifier {}", v);
 
-        Ok(())
+                    if next == Some(Token::LeftParen) {
+                        self.lexer.next();
+                        let index = self.parse()?;
+                        if self.lexer.peek() == Some(&Token::RightParen) {
+                            let res = Ok(LValue::ArrayElement {
+                                variable,
+                                index: Box::new(index.unwrap()),
+                            });
+
+                            self.lexer.next();
+
+                            res
+                        } else {
+                            Err(Error {
+                                kind: ErrorKind::MismatchedParentheses,
+                                line: 0, // TODO
+                            })
+                        }
+                    } else {
+                        Ok(LValue::Variable(variable))
+                    }
+                }
+                _ => {
+                    // println!("expected identifier");
+                    Err(Error {
+                        kind: ErrorKind::ExpectedIdentifier,
+                        line: 0, // TODO
+                    })
+                }
+            }
+        }
+
+        fn term(&mut self) -> Result<Option<Expression>, Error> {
+            match self.lexer.peek_mut() {
+                Some(Token::Number(n)) => {
+                    let res = Ok(Some(Expression::Number(*n)));
+                    self.lexer.next();
+                    res
+                }
+                Some(Token::Identifier(_)) => self.lvalue().map(|v| Some(Expression::LValue(v))),
+                Some(Token::String(s)) => {
+                    let res = Ok(Some(Expression::String(mem::take(s))));
+                    self.lexer.next();
+                    res
+                }
+                Some(Token::LeftParen) => {
+                    self.lexer.next();
+                    let res = self.parse()?;
+                    if self.lexer.peek() == Some(&Token::RightParen) {
+                        self.lexer.next();
+                        Ok(res)
+                    } else {
+                        Err(Error {
+                            kind: ErrorKind::MismatchedParentheses,
+                            line: 0, // TODO
+                        })
+                    }
+                }
+                _ => Ok(None),
+            }
+        }
+
+        // // unary + and -
+        fn factor(&mut self) -> Result<Option<Expression>, Error> {
+            // println!("factor");
+            if self.lexer.peek() == Some(&Token::Plus) || self.lexer.peek() == Some(&Token::Minus) {
+                let op = match self.lexer.next() {
+                    Some(Token::Plus) => UnaryOperator::Plus,
+                    Some(Token::Minus) => UnaryOperator::Minus,
+                    _ => unreachable!(),
+                };
+
+                let operand = self.factor();
+                let operand = if let Some(operand) = operand? {
+                    operand
+                } else {
+                    return Err(Error {
+                        kind: ErrorKind::ExpectedExpression,
+                        line: 0, // TODO
+                    });
+                };
+
+                Ok(Some(Expression::Unary {
+                    op,
+                    operand: Box::new(operand),
+                }))
+            } else {
+                self.term()
+            }
+        }
+
+        fn mul_div(&mut self) -> Result<Option<Expression>, Error> {
+            let mut left = if let Some(left) = self.factor()? {
+                left
+            } else {
+                return Ok(None);
+            };
+
+            while let Some(&Token::Star) | Some(&Token::Slash) = self.lexer.peek() {
+                let op = match self.lexer.next() {
+                    Some(Token::Star) => BinaryOperator::Mul,
+                    Some(Token::Slash) => BinaryOperator::Div,
+                    _ => unreachable!(),
+                };
+
+                let right = self.factor();
+                let right = if let Some(right) = right? {
+                    right
+                } else {
+                    return Err(Error {
+                        kind: ErrorKind::ExpectedExpression,
+                        line: 0, // TODO
+                    });
+                };
+
+                left = Expression::Binary {
+                    left: Box::new(left),
+                    op,
+                    right: Box::new(right),
+                };
+            }
+
+            Ok(Some(left))
+        }
+
+        fn add_sub(&mut self) -> Result<Option<Expression>, Error> {
+            let mut left = if let Some(left) = self.mul_div()? {
+                left
+            } else {
+                return Ok(None);
+            };
+
+            while let Some(&Token::Plus) | Some(&Token::Minus) = self.lexer.peek() {
+                println!("add_sub");
+
+                let op = match self.lexer.next() {
+                    Some(Token::Plus) => BinaryOperator::Add,
+                    Some(Token::Minus) => BinaryOperator::Sub,
+                    _ => unreachable!(),
+                };
+
+                let right = self.add_sub();
+                let right = if let Some(right) = right? {
+                    right
+                } else {
+                    return Err(Error {
+                        kind: ErrorKind::ExpectedExpression,
+                        line: 0, // TODO
+                    });
+                };
+
+                left = Expression::Binary {
+                    left: Box::new(left),
+                    op,
+                    right: Box::new(right),
+                };
+            }
+
+            Ok(Some(left))
+        }
+
+        fn comparison(&mut self) -> Result<Option<Expression>, Error> {
+            let mut left = if let Some(left) = self.add_sub()? {
+                left
+            } else {
+                return Ok(None);
+            };
+
+            while let Some(&Token::Equal)
+            | Some(&Token::Diamond)
+            | Some(&Token::LessThan)
+            | Some(&Token::LessOrEqual)
+            | Some(&Token::GreaterThan)
+            | Some(&Token::GreaterOrEqual) = self.lexer.peek()
+            {
+                let op = match self.lexer.next() {
+                    Some(Token::Equal) => BinaryOperator::Eq,
+                    Some(Token::Diamond) => BinaryOperator::Ne,
+                    Some(Token::LessThan) => BinaryOperator::Lt,
+                    Some(Token::LessOrEqual) => BinaryOperator::Le,
+                    Some(Token::GreaterThan) => BinaryOperator::Gt,
+                    Some(Token::GreaterOrEqual) => BinaryOperator::Ge,
+                    _ => unreachable!(),
+                };
+
+                let right = self.comparison();
+                let right = if let Some(right) = right? {
+                    right
+                } else {
+                    return Err(Error {
+                        kind: ErrorKind::ExpectedExpression,
+                        line: 0, // TODO
+                    });
+                };
+
+                left = Expression::Binary {
+                    left: Box::new(left),
+                    op,
+                    right: Box::new(right),
+                };
+            }
+
+            Ok(Some(left))
+        }
+    }
+
+    #[cfg(test)]
+    mod tests {
+        use super::*;
+
+        #[test]
+        fn test_add_sub_1() -> Result<(), Error> {
+            let expected = Expression::Binary {
+                left: Box::new(Expression::Number(1)),
+                op: BinaryOperator::Add,
+                right: Box::new(Expression::Binary {
+                    left: Box::new(Expression::Number(2)),
+                    op: BinaryOperator::Sub,
+                    right: Box::new(Expression::Number(3)),
+                }),
+            };
+
+            let lexer = Lexer::new("1 + 2 - 3");
+            let mut parser = ExpressionParser::new(lexer.peekable());
+
+            let res = parser.add_sub()?;
+
+            if let Some(res) = res {
+                assert_eq!(res, expected, "Expected: {:?}, got: {:?}", expected, res);
+            } else {
+                return Err(Error {
+                    kind: ErrorKind::ExpectedExpression,
+                    line: 0,
+                });
+            }
+
+            Ok(())
+        }
+
+        // FIXME: Check operator precedence
+        #[test]
+        fn test_mul_div_1() -> Result<(), Error> {
+            let expected = Expression::Binary {
+                left: Box::new(Expression::Number(1)),
+                op: BinaryOperator::Mul,
+                right: Box::new(Expression::Binary {
+                    left: Box::new(Expression::Number(2)),
+                    op: BinaryOperator::Div,
+                    right: Box::new(Expression::Number(3)),
+                }),
+            };
+
+            let lexer = Lexer::new("1 * 2 / 3");
+            let mut parser = ExpressionParser::new(lexer.peekable());
+
+            let res = parser.mul_div()?;
+
+            if let Some(res) = res {
+                assert_eq!(res, expected, "Expected: {:?}, got: {:?}", expected, res);
+            } else {
+                return Err(Error {
+                    kind: ErrorKind::ExpectedExpression,
+                    line: 0,
+                });
+            }
+
+            Ok(())
+        }
+
+        #[test]
+        fn test_lvalue_1() -> Result<(), Error> {
+            let expected = LValue::Variable("A".to_owned());
+
+            let lexer = Lexer::new("A");
+            let mut parser = ExpressionParser::new(lexer.peekable());
+
+            let res = parser.lvalue()?;
+
+            assert_eq!(res, expected, "Expected: {:?}, got: {:?}", expected, res);
+
+            Ok(())
+        }
     }
 }
